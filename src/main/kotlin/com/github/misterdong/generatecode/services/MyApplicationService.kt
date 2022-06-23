8b98241f11ab64b516f66e95dea4811de063a148
GitHub Action <action@github.com>
2022-06-23 04:11:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/misterdong/generatecode/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/misterdong/generatecode/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.misterdong.generatecode.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.misterdong.generatecode.MyBundle
 
 class MyApplicationService {
 
